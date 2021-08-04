d7fe6ead607d37bc37e1ec6aa046cd776d22c80a
masakinihirota <masakinihirota@gmail.com>
2021-08-04 12:24:30 +0900

    fixed Eslint Warning message Link

--- src/pages/api/examples/jwt.js
+++ src/pages/api/examples/jwt.js
@@ -3,6 +3,7 @@ import jwt from 'next-auth/jwt'
 
 const secret = process.env.SECRET
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   const token = await jwt.getToken({ req, secret })
   res.send(JSON.stringify(token, null, 2))
--- src/pages/api/examples/protected.js
+++ src/pages/api/examples/protected.js
@@ -1,6 +1,7 @@
 // This is an example of to protect an API route
 import { getSession } from 'next-auth/client'
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
   const session = await getSession({ req })
 
