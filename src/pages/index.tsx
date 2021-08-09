2fce464ac8a8cd4a1e56879679e62b554ce31595
masakinihirota <masakinihirota@gmail.com>
2021-08-09 23:54:11 +0900

    オアシス宣言

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,79 @@
+import Link from 'next/link'
 import Layout from '../components/layout'
 
-export default function Page () {
+export default function Page() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
+      <h1>2021年8月9日</h1>
+      <h1>真っ先に拾った（α版）</h1>
+      <h2>github:public</h2>
+      <h5>
+        インターネットという情報の洪水から、真っ先に大切で価値ある情報を拾い上げ、宝物を共有しグループを作り上げていくサービス。
+      </h5>
+      <h6>VNS=value network service</h6>
+      <h2>
+        <a href="/oasisDeclaration">オアシス宣言</a>
+      </h2>
+      <h4>このサイトはオアシス宣言に従います。</h4>
+      <p></p>
+      <h3>ユーザー名:masakinihirota（固定）</h3>
+      <h3>ユーザーの製作物（production）</h3>
       <p>
-        This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
+        <Link href="/">
+        <a >真っ先に拾った（ウェブサービス）</a>
+        </Link>
+      </p>
+      <p></p>
+      <h3>プロフィール（個人の価値観）</h3>
+      <div>
+        <Link href={'/category/manga/'}>
+          <a>漫画</a>
+        </Link>
+        <p>
+          愛読書:
+          ワンピース、進撃の巨人、龍と苺、りこさんブッチギリです、四月は君の嘘、新上がってなンボ！！
+          太一よ泣くな、鈴木先生、ここは今から倫理です。、壬生義士伝、アスペル・カノジョ、ムーンランド、etc
+        </p>
+        <p>
+          尊敬している作家:
+          尾田栄一郎、諫山創、渡辺多恵子、ちばてつや、高橋留美子、大島弓子、遠藤淑子、新川直司、etc
+        </p>
+        <p></p>
+        <br />
+        <Link href={'/category/anime/'}>
+          <a>アニメ</a>
+        </Link>
+        <p>
+          2020年秋アニメ:未来少年コナン、メジャーセカンド2、ドラゴンクエスト
+          ダイの大冒険、魔王城でおやすみ、兄に付ける薬はない！4
+        </p>
+        <p>
+          2020年夏アニメ:バキ
+          -大擂台賽編-、宇崎ちゃんは遊びたい！、とある科学の超電磁砲T、GREAT
+          PRETENDER
+        </p>
+        <br />
+        <Link href={'/category/music/'}>
+          <a>音楽</a>
+        </Link>
+        <br />
+        <Link href={'/category/movie/'}>
+          <a>映画</a>
+        </Link>
+        <br />
+        <Link href={'/category/etc/'}>
+          <a>その他</a>
+        </Link>
+        <br />
+        <p></p>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
+      <br />
+      <p>
+        This is an example site to demonstrate how to use{' '}
+        <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
     </Layout>
   )
---
+++ src/pages/oasisDeclaration.tsx
@@ -0,0 +1,79 @@
+// import Link from "next/link";
+// import Layout from "../components/MyLayout.js";
+
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout title="オアシス宣言">
+    <h1>オアシス宣言</h1>
+    <p>
+      オアシス宣言
+      <br />
+      インターネットという情報の洪水の中で、オアシスの場所であることを宣言します。
+      <br />
+      <br />
+      モットー：褒めるときは大きな声でみんなの前で、叱るときは二人きりで小さな声で。
+      <br />
+      <br />
+      1
+      インターネット上で翼を休める場所、砂漠の中で命の水を授かる場所を作ります。
+      <br />
+      2 広告はユーザー側に主導権があります。
+      <br />
+      3 共通の価値観を持った人々のオアシスという場所を作ります
+      <br />
+      4 お互いの価値観を認めるのならば、誰もが参加できます。
+      <br />
+      5 きれいな世界、優しい世界を守り、広めます。
+      <br />
+      誰もが争うことなく休憩する場所であることを目指します。
+      <br />
+      誰もが笑顔になれる場所です。
+      <br />
+      <br />
+      草稿
+      <br />
+      <br />
+      google translate
+      <br />
+      <br />
+      Oasis Declaration
+      <br />
+      In the flood of information called the Internet, we declare that it is an
+      oasis place.
+      <br />
+      <br />
+      Motto: When giving up, in front of everyone in a loud voice, when
+      speaking, with only two people and a small voice.
+      <br />
+      <br />
+      1 Create a place where you can rest your wings on the Internet, and a
+      place where you can receive water of life in the desert.
+      <br />
+      2 Advertising is led by the user.
+      <br />
+      3 Create an oasis of people with common values
+      <br />
+      4 Everyone can participate if they recognize each other&apos;s values.
+      <br />
+      5 Protect and promote a beautiful world, a kind world.
+      <br />
+      We aim to be a place where everyone can rest without conflict.
+      <br />
+      It is a place where everyone can smile.
+      <br />
+      <br />
+      draft
+      <br />
+      <br />
+    </p>
+    <div>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  </Layout>
+)
+
+export default IndexPage
